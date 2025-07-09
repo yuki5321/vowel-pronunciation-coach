6a725e29b7adebb0f886d8661858e1ebd8f19b5d
yuki5321 <y.takeuchi1104@gmail.com>
2025-07-09 19:02:30 +0900

    コミット１０

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // App.jsを読み込む
+
+// public/index.html の <div id="root"></div> を見つける
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Appコンポーネントを描画する
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
